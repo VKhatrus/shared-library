8fa1a2e66dde6b5d486dc4ad7946047fd6454c6d
Your Name <vitaliy.khatrus@gmail.com>
2021-05-04 09:18:17 +0300

    add some changes in gitTag

--- vars/gitTag.groovy
+++ vars/gitTag.groovy
@@ -4,11 +4,14 @@ def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
+    //body()
+    echo "\u001B[32mINFO: Publishing...\u001B[m"
     body()
+    echo "\u001B[32mINFO: End Publish...\u001B[m"
 
-    sh "git config user.email vitaliy.khatrus@ardas.biz"
-    sh "git config user.name Vitaliy Khatrus"
+//    sh "git config user.email vitaliy.khatrus@ardas.biz"
+//    sh "git config user.name Vitaliy Khatrus"
 
-    sh "git tag -fa v${config.releaseVersion} -m 'Release version ${config.releaseVersion}'"
-    sh "git push origin v${config.releaseVersion}"
+//    sh "git tag -fa v${config.releaseVersion} -m 'Release version ${config.releaseVersion}'"
+//    sh "git push origin v${config.releaseVersion}"
 }
