ea04cd692eb41e369d0ad86469cd235cf4096ab1
Your Name <vitaliy.khatrus@gmail.com>
2021-05-04 09:22:28 +0300

    add some changes in gitTag

--- vars/gitTag.groovy
+++ vars/gitTag.groovy
@@ -9,6 +9,21 @@ def call(body) {
     body()
     echo "\u001B[32mINFO: End Publish...\u001B[m"
 
+   // In the bar DSL element
+    echo 'I am bar'
+
+    // Expecting a script element as a closure. The insanceof needs script approvals
+    //assert config.script != null, 'A script element was not supplied'
+    //assert config.script instanceof Closure, 'The script element supplied must be a closure'
+
+    // Call the script closure
+    config.script.delegate = this
+    config.script.resolveStrategy = Closure.DELEGATE_FIRST
+    def result = config.script.call()
+
+    // Returning the script result
+    return result
+
 //    sh "git config user.email vitaliy.khatrus@ardas.biz"
 //    sh "git config user.name Vitaliy Khatrus"
 
