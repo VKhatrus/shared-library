60d00fa8765382159a67eb8e3f0b5dd1f52a7d80
Your Name <vitaliy.khatrus@gmail.com>
2021-05-18 16:31:19 +0300

    add options in dockerBuild lib

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,7 @@
-//def call(String project, String hubUser) {
-def call() {
+def call(String project, String hubUser) {
+//def call() {
 //    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    sh "docker image build -t ${hubUser}/${project}:${env.RELEASE_VERSION} ."
     withCredentials([usernamePassword(
             credentialsId: "docker",
             usernameVariable: "USER",
@@ -8,5 +9,5 @@ def call() {
     )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
-//    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+    sh "docker image push ${hubUser}/${project}:${env.RELEASE_VERSION}"
 }
